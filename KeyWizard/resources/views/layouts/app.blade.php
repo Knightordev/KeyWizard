f76294d0e76db7321f233570c7166e50fee15df2
Héctor Iván Chumba Poot <hichumba@gmail.com>
2026-06-05 12:20:14 -0600

    basic template and welcome

---
+++ KeyWizard/resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi Sitio</title>
+</head>
+<body>
+
+    <header>
+        <h1>Mi Sistema</h1>
+    </header>
+
+    <nav>
+        Menú
+    </nav>
+
+    <main>
+        @yield('contenido')
+    </main>
+
+    <footer>
+        Copyright 2026
+    </footer>
+
+</body>
+</html>
